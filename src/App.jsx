6e4b769c3a6f9e75546b38944811988beb1f9d4a
Maximiliano Blanco <inkcoming@protonmail.com>
2020-07-12 13:26:03 -0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './App.scss';
+
+const App = () => {
+    return <div className="App">React + Parcel</div>;
+};
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Root from './Root';
+import './index.scss';
+
+ReactDOM.render(<Root />, document.getElementById('root'));
